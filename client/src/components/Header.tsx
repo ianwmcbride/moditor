56f81b5c637b73ef64e56d30a52181625c269b9a
Ian McBride <IanMcBride777@gmail.com>
2026-08-19 20:11:32 -0500

    remove breadcrumbs component (for now)

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,6 @@
-import type { Crumb } from "../types"
-import Breadcrumbs from "./Breadcrumbs"
-
-export default function Header(crumbs: Crumb[]) {
+export default function Header() {
     return (
         <header>
-            {crumbs.length > 0 && crumbs.map(c => <a href={c.href}>{c.text}</a>)}
             <div id="logo-container">
                 <h1><a href="/">Moditor</a></h1>
             </div>
